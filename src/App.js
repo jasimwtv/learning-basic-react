4e606f00bee0f28633627ee73d939e9b586bf70a
jasimwtv <jasim.uddin@wtvglobal.com>
2022-05-25 20:29:30 +0600

    initial

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {useState} from 'react';
 
 function App() {
+  const [value, setValue] = useState({
+    name: "asdf",
+    email: "",
+    role: "",
+  });
+
+  const handleInput = (e) => {
+    const { name, value } = e.target;
+
+    if(value) {
+      setValue(state => ({
+        ...state,
+        [name]: value
+      }))
+    }
+
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <form>
+          <input name="name" placeholder="Name" type="text" onChange={handleInput} />
+          <input name="email" placeholder="Email" type="email" onChange={handleInput} />
+          <input type="text" placeholder="Role" name="role" onChange={handleInput} />
+        </form>
+        <div>
+          Information
+          <h2>{value.name}</h2>
+          <p>{value.email}</p>
+          <p>{value.role}</p>
+        </div>
     </div>
   );
 }

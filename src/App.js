d4fa3cdd31131d1367e4a0f2e6361bd9d248a5bb
jasimwtv <jasim.uddin@wtvglobal.com>
2022-06-07 14:17:56 +0600

    API call

--- src/App.js
+++ src/App.js
@@ -1,38 +1,80 @@
 import './App.css';
-import React, {useState} from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
 function App() {
-  const [value, setValue] = useState({
-    name: "asdf",
-    email: "",
-    role: "",
-  });
 
-  const handleInput = (e) => {
-    const { name, value } = e.target;
+  const [users, setUsers] = useState([]);
 
-    if(value) {
-      setValue(state => ({
-        ...state,
-        [name]: value
-      }))
-    }
+  // API call using XMLHttpRequest
+  // useEffect(() => {
+  //   let request = new XMLHttpRequest();
+  //   request.open("GET", "https://jsonplaceholder.typicode.com/users");
+  //   request.send();
+  //   request.onload = () => {
+  //     console.log("REQUEST", request);
+  //     if(request.status === 200) {
+  //       const userList = JSON.parse(request.response);
+  //       setUsers(userList);
+  //     }
+  //   }
+  // }, []);
 
+  // API call using fetch .then method
+  // useEffect(() => {
+  //   fetch("", 'https://jsonplaceholder.typicode.com/users')
+  //   .then(res => {
+  //     return res.json();
+  //   }).then(res => {
+  //     const userList = res
+  //     setUsers(userList);
+  //   }).catch(err => {
+  //     console.log("ERROR", err);
+  //   })
+  // }, [])
+
+  // const getUserData = async() => {
+  //   const resposne =  await fetch('https://jsonplaceholder.typicode.com/users');
+  //   const data = await resposne.json();
+  //   console.log("DATA", data)
+  //   setUsers(data);
+  // }
+
+  const getUserData = async() => {
+    const response =  await axios.get('https://jsonplaceholder.typicode.com/users');
+    const data = await response.data;
+    setUsers(data);
   }
+
+  useEffect(() => {
+    getUserData();
+  }, [])
+
+  // axios.get("https://jsonplaceholder.typicode.com/users")
+  // .then(res => {
+  //   setUsers(res.data);
+  // }).catch(err => {
+  //   alert("ERROR");
+  // });
 
   return (
     <div className="App">
-        <form>
-          <input name="name" placeholder="Name" type="text" onChange={handleInput} />
-          <input name="email" placeholder="Email" type="email" onChange={handleInput} />
-          <input type="text" placeholder="Role" name="role" onChange={handleInput} />
-        </form>
-        <div>
-          Information
-          <h2>{value.name}</h2>
-          <p>{value.email}</p>
-          <p>{value.role}</p>
-        </div>
+        <table>
+          <tr>
+            <th>Name</th>
+            <th>Email</th>
+            <th>Address</th>
+          </tr>
+          {users?.map((item) => (
+            <div key={item.id}>
+              <tr>
+                <td>{item.name}</td>
+                <td>{item.email}</td>
+                <td> {item.address.city} </td>
+              </tr>
+            </div>
+          ))}
+        </table>
     </div>
   );
 }

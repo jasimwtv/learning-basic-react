954aa88d412f2a3e034f5b0b11e8b72ff95acb9e
jasimwtv <jasim.uddin@wtvglobal.com>
2022-06-07 14:51:32 +0600

    update

--- src/App.js
+++ src/App.js
@@ -60,20 +60,22 @@ function App() {
   return (
     <div className="App">
         <table>
-          <tr>
-            <th>Name</th>
-            <th>Email</th>
-            <th>Address</th>
-          </tr>
-          {users?.map((item) => (
-            <div key={item.id}>
-              <tr>
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Email</th>
+              <th>Address</th>
+            </tr>
+          </thead>
+          <tbody>
+            {users?.map((item) => (
+              <tr key={item.id}>
                 <td>{item.name}</td>
                 <td>{item.email}</td>
                 <td> {item.address.city} </td>
               </tr>
-            </div>
-          ))}
+            ))}
+          </tbody>
         </table>
     </div>
   );
